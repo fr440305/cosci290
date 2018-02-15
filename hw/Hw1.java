c4190396056e60c6eda59f45af60e9bb945ca0c1
fr440305 <fr440305@gmail.com>
2018-02-15 18:18:38 -0500

    ...

---
+++ hw/Hw1.java
@@ -0,0 +1,7 @@
+public class Hw1 {
+	public static void main (String[] args) {
+		System.out.println("");
+		System.out.println("Zombie Survival");
+		System.out.println("");
+	}
+}
---
+++ lect/DemoBoolean.java
@@ -0,0 +1,24 @@
+/*
+Weishu Tan
+2018-2-15
+
+Overview
+
+Builtin operators:
+
+ - double-plus, double-minus (++, --)
+ - op-eq (+= -= *= /= %= ...)
+ - Type conversion
+   - Syntax  (new_type)tar_varpai
+	 - ..
+
+*/
+
+
+
+public class DemoBoolean {
+	public static void main (String[] args) {
+		int num = 1;
+		num = num + 1;
+	}
+}
