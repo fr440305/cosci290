687ea71b649fe8906de2d8cdecfdb3c31c6b1075
fr440305 <fr440305@gmail.com>
2018-02-16 04:42:43 -0500

    auto commit

--- pj/proto/Main.java
+++ pj/proto/Main.java
@@ -7,3 +7,23 @@ public class Main {
 class Zombie {
 	public int hp;
 }
+
+class Sence {
+}
+
+// eg, currency, blood-bag, etc.
+class Tool {
+}
+
+class Player {
+}
+
+class Worrior extends Player {
+}
+
+class Healer extends Player {
+}
+
+// multi-player
+class Net {
+}
