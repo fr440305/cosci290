df3424ed1e90ac79c88c25e99f19ee71aaf64465
fr440305 <fr440305@gmail.com>
2018-03-08 18:15:25 -0500

    auto commit

---
+++ custom/ChristianCastillo/Spider.java
@@ -0,0 +1,16 @@
+// Author: Weishu Tan (cosci 290 group 4)
+// Manager: Christian Castillo (Could you please write your name here?)
+
+public class Spider {
+  private health;
+  private attack;
+  private defense;
+  public Spider () {
+    this.health = 15;
+    this.attack = 5;
+    this.defense = 5;
+  }
+  public hurtAlien (Alien a) {
+    // todo
+  }
+}
---
+++ lab/7/README.txt
@@ -0,0 +1,10 @@
+Lab 7 of group 4
+
+Create custom classes for members in group 3.
+Create a folder called "custom" and put custom
+classes created for other students in this folder.
+
+For each custom class, include in a block comment
+at the beginning of the class that you are the
+author and for which student/group the custom class
+is created for.
