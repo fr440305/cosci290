1bafc06faae990ba29045d2d1ec5803bcd855429
fr440305 <fr440305@gmail.com>
2018-03-08 18:44:10 -0500

    auto commit

--- custom/ChristianCastillo/Spider.java
+++ custom/ChristianCastillo/Spider.java
@@ -1,16 +1,49 @@
 // Author: Weishu Tan (cosci 290 group 4)
-// Manager: Christian Castillo (Could you please write your name here?)
+// Manager: Christian Castillo
 
 public class Spider {
-  private health;
-  private attack;
-  private defense;
+  
+  private int health;
+  private int attack;
+  private int defense;
+  
   public Spider () {
     this.health = 15;
     this.attack = 5;
     this.defense = 5;
   }
-  public hurtAlien (Alien a) {
-    // todo
+  
+  public int getHealth () {
+    return this.health;
   }
+  
+  public int getAttack () {
+    return this.attack;
+  }
+  
+  public int getDefense () {
+    return this.defense;
+  }
+  
+  public void setHealth (int h) {
+    this.health = h;
+  }
+  
+  public void setAttack (int a) {
+    this.attack = a;
+  }
+  
+  public void setDefense (int d) {
+    this.defense = d;
+  }
+  
+  public String toString() {
+    return
+      "I am a spider. My informations:\n" +
+      "\t health " + this.health +
+      "\t attack " + this.attack + 
+      "\t defense " + this.defense;
+  }
+  
+  
 }
