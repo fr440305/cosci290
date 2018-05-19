f4ce39032f94d98aa8ab7b3b1b4dc1f678acaa4a
fr440305 <fr440305@gmail.com>
2018-05-18 23:58:26 -0700

    p/p :: .

--- proj/proto/Main.java
+++ proj/proto/Main.java
@@ -1,25 +1,12 @@
 // weishu tan
 // 2018-5-5
 
-// Summary
-//
-// - storymap.txt
-// - StoryName
-// - StoryTitle
-// - Progress
-// - Choice
-// - StoryMap : HashMap<String, HashMap<String, String>>
-// - Nexts : HashMap<String, String>
-
-// stdin:
 import java.util.Scanner;
 
-// storymap:
 import java.util.Map;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-// fileio:
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -29,109 +16,28 @@ import java.io.IOException;
 
 public class Main {
 
-	// This function read ./storymap.txt and returns a StoryMap.
-	public static HashMap<String, HashMap<String, String>> mapStories(String smp) {
-		// smp : story map path
-		HashMap<String, HashMap<String, String>> ret =
-			new HashMap<String, HashMap<String, String>>();
-
+	public static ArrayList<String> readFile(String filepath) {
+		ArrayList<String> lines = new ArrayList< >();
 		try {
-			BufferedReader buf = new BufferedReader(new FileReader(smp));
-			String line;
-			int linenum = 1;
-			String currentStory = "";
-			HashMap<String, String> nexts = null;
-			while ((line = buf.readLine()) != null) {
-				if (line.endsWith("{")){
-					nexts = new HashMap<String, String>();
-					currentStory = line.substring(0, line.length() - 2);
-				} else if (line.contains("->")) { // choice -> nextStory
-					if (nexts == null || currentStory.length() == 0) {
-						System.out.println("[ERR] :: At storymap.txt line " + linenum);
-						return null;
-					}
-					String choice = line.split("->")[0].trim();
-					String nextStory = line.split("->")[1].trim();
-					nexts.put(choice, nextStory);
-				} else if (line.equals("}")) {
-					if (nexts == null || currentStory.length() == 0) {
-						System.out.println("[ERR] :: At storymap.txt line " + linenum);
-						return null;
-					}
-					ret.put(currentStory, nexts);
-				}
-				linenum += 1;
+			BufferedReader reader = new BufferedReader(new FileReader(filepath));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				lines.add(line);
 			}
-			buf.close();
-			return ret;
+			reader.close();
+			return lines;
 		} catch (FileNotFoundException errnf) {
-			errnf.printStackTrace();
+			System.out.println("Error : File " + filepath + " Not Found!");
 		} catch (IOException errio) {
-			errio.printStackTrace();
+			System.out.println("Error : Cannot Read File " + filepath + "!");
 		}
 		return null;
 	}
 
-	public static String filePath(String title) {
-		return "story/" + title.replaceAll(" ", ".") + ".txt";
-	}
-
-	public static String showStory(String title) {
+	public static String writeFile(String filepath, String content) {
 		try {
-			BufferedReader buf = new BufferedReader(new FileReader(filePath(title)));
-			saveProgress(title);
-			String line;
-			while ((line = buf.readLine()) != null) {
-				line = line.trim();
-				if (line.length() == 0) { // jump
-					System.out.println("");
-				} else if (line.startsWith("<")) { // no-indent
-					System.out.println(line.substring(1).trim());
-				} else if (line.startsWith("#")) { // comment
-					continue;
-				} else if (line.startsWith(".")) {
-					System.out.println("  " + line.substring(1));
-				} else if (line.equals("@pause")) {
-					new Scanner(System.in).nextLine();
-				} else if (line.startsWith("@n ")) { // newline
-					int n = Integer.parseInt(line.substring(2).trim());
-					for (int i = 0; i < n; i++) {
-						System.out.println("");
-					}
-				} else {
-					System.out.print("  " + line);
-					new Scanner(System.in).nextLine();
-				}
-			}
-			buf.close();
-		} catch (FileNotFoundException errnf) {
-			return "nofile";
-		} catch (IOException errio) {
-			return "errio";
-		}
-		return null;
-	}
-  
-	public static String getProgress(HashMap<String, HashMap<String, String>> g) {
-		try {
-			String ret = new BufferedReader(new FileReader("data/progress.txt")).readLine();
-			if (ret != null && g.containsKey(ret)) {
-				return ret; 
-			} else {
-				System.out.println("Cannot read the progress.");
-			}
-		} catch (Exception err) { // not a big deal
-			System.out.println("Cannot read the progress.");
-		}
-		//error or first time:
-		return "tutorial";
-	}
-  
-	// Delete progress. Return error.
-	public static String delProgress() {
-		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("data/progress.txt"));
-			bw.write("");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(filepath));
+			bw.write(content);
 			bw.close();
 			return null;
 		} catch (Exception err) {
@@ -140,21 +46,96 @@ public class Main {
 		return "error";
 	}
 
-	// Return error.
-	public static String saveProgress(String storyName) {
-		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("data/progress.txt"));
-			bw.write(storyName);
-			bw.close();
-			return null;
-		} catch (Exception err) {
-			System.out.println("saveProgress Error!" + err.toString());
+	// This function read ./storymap.txt and returns a StoryMap.
+	public static HashMap<String, HashMap<String, String>> mapStories(String smp) {
+		// smp : story map path
+		HashMap<String, HashMap<String, String>> storyMap = new HashMap< >();
+		int linenum = 1;
+		String currentStory = "";
+		HashMap<String, String> nexts = null;
+		ArrayList<String> file = readFile(smp);
+		if (file == null) return null;
+
+		for (String line : file) {
+			if (line.endsWith("{")){
+				nexts = new HashMap<String, String>();
+				currentStory = line.substring(0, line.length() - 2);
+			} else if (line.contains("->")) { // choice -> nextStory
+				if (nexts == null || currentStory.length() == 0) {
+					System.out.println("[ERR] :: At storymap.txt line " + linenum);
+					return null;
+				}
+				String choice = line.split("->")[0].trim();
+				String nextStory = line.split("->")[1].trim();
+				nexts.put(choice, nextStory);
+			} else if (line.equals("}")) {
+				if (nexts == null || currentStory.length() == 0) {
+					System.out.println("[ERR] :: At storymap.txt line " + linenum);
+					return null;
+				}
+				storyMap.put(currentStory, nexts);
+			}
+			linenum += 1;
 		}
-		return "error";
+		return storyMap;
+	}
+
+	public static String storyPath(String title) {
+		return "story/" + title.replaceAll(" ", ".") + ".txt";
+	}
+
+	public static String showStory(String title) {
+		saveProgress(title);
+		ArrayList<String> file = readFile(storyPath(title));
+		if (file == null) return "error";
+		for (String line : file) {
+			line = line.trim();
+			if (line.length() == 0) { // jump
+				System.out.println("");
+			} else if (line.startsWith("<")) { // no-indent
+				System.out.println(line.substring(1).trim());
+			} else if (line.startsWith("#")) { // comment
+				continue;
+			} else if (line.startsWith(".")) {
+				System.out.println("  " + line.substring(1));
+			} else if (line.equals("@pause")) {
+				new Scanner(System.in).nextLine();
+			} else if (line.startsWith("@n ")) { // newline
+				int n = Integer.parseInt(line.substring(2).trim());
+				for (int i = 0; i < n; i++) {
+					System.out.println("");
+				}
+			} else {
+				System.out.print("  " + line);
+				new Scanner(System.in).nextLine();
+			}
+		}
+		return null;
+	}
+  
+	public static String getProgress(HashMap<String, HashMap<String, String>> g) {
+		ArrayList<String> file = readFile("data/progress.txt");
+		String ret;
+		if (file != null && file.size() >= 1 && g.containsKey(ret = file.get(0))) {
+			return ret;
+		} else {
+			System.out.println("Cannot read the progress.");
+			return "tutorial";
+		}
+	}
+
+	// Delete progress. Return error.
+	public static void delProgress() {
+		writeFile("data/progress.txt", "");
+	}
+
+	// Return error.
+	public static void saveProgress(String storyTitle) {
+		writeFile("data/progress.txt", storyTitle);
 	}
 
 	public static String chooseRandom(HashMap<String, String> nexts) {
-		ArrayList<String> choices = new ArrayList<String>();
+		ArrayList<String> choices = new ArrayList< >();
 		for (Map.Entry<String, String> entry : nexts.entrySet()) {
 			choices.add(entry.getKey());
 		}
@@ -181,7 +162,7 @@ public class Main {
 	public static void main(String[] args) {
 		HashMap<String, HashMap<String, String>> g = mapStories("storymap.txt");
 		String currentStory = getProgress(g);
-		// "Game Loop": 
+		// the game loop:
 		while (true) {
 			HashMap<String, String> nexts = g.get(currentStory);
 			String err = showStory(currentStory);
